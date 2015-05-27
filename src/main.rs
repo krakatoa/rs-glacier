a76f7f5970482ad89f23282e7d24c978ec0a6e9d
Fernando Alonso <krakatoa1987@gmail.com>
2015-05-27 08:55:53 -0300

    Simple http endpoints to present web files

--- src/main.rs
+++ src/main.rs
@@ -9,53 +9,42 @@ use hyper::server::Request;
 use hyper::server::Response;
 use hyper::net::Fresh;
 
-//lmdb
+use hyper::{Get, Post};
+use hyper::uri::RequestUri::AbsolutePath;
 
-extern crate lmdb_rs as lmdb;
+fn hello(req: Request, mut res: Response) {
+  let settings = include_bytes!("../static/settings.png");
+  let index_html = include_bytes!("../static/index.html");
 
-use std::path::Path;
-use lmdb::{EnvBuilder, DbFlags};
+  match req.uri {
+    AbsolutePath(ref path) => match (&req.method, &path[..]) {
+      (&Get, "/index.html") => {
+        res.send(index_html);
+        return;
+      },
+      (&Get, "/settings.png") => {
+        res.send(settings);
+        return;
+      },
+      /*(&Post, "/index.html") => {
+        *res.status_mut() = hyper::NotFound;
+        println!("should fail");
+        return;
+      },
+      (&Post, "/echo") => (), // fall through, fighting mutable borrows
+      */
+      _ => {
+        *res.status_mut() = hyper::NotFound;
+        return;
+      }
+    },
+    _ => {
+      return;
+    }
+  };
 
-fn hello(_: Request, res: Response<Fresh>) {
   let mut res = res.start().unwrap();
-  // res.write_all(b"Hello World!").unwrap();
-  let name = read();
-  // println!("It's {:?} Smith", name);
-  res.write_all(name.as_bytes());
-  res.end().unwrap();
-}
-
-fn read() -> String {
-  let path = Path::new("test-lmdb");
-  let mut env = EnvBuilder::new().open(&path, 0o777).unwrap();
-
-  let db_handle = env.get_default_db(DbFlags::empty()).unwrap();
-  // let txn = env.new_transaction().unwrap();
-  // {
-  //     let db = txn.bind(&db_handle); // get a database bound to this transaction
-
-  //     let pairs = vec![("Albert", "Einstein",),
-  //                      ("Joe", "Smith",),
-  //                      ("Jack", "Daniels")];
-
-  //     for &(name, surname) in pairs.iter() {
-  //         db.set(&surname, &name).unwrap();
-  //     }
-  // }
-
-  // // Note: `commit` is choosen to be explicit as
-  // // in case of failure it is responsibility of
-  // // the client to handle the error
-  // match txn.commit() {
-  //     Err(_) => panic!("failed to commit!"),
-  //     Ok(_) => ()
-  // }
-
-  let reader = env.get_reader().unwrap();
-  let db = reader.bind(&db_handle);
-  let name = db.get::<&str>(&"Smith");
-  // println!("It's {} Smith", name);
-  String::from(name.unwrap())
+  // res.end().unwrap();
 }
 
 fn main() {
